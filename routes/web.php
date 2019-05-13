2f20281c304a1b6335e0cb0927616fe1637d386c
masami tawara <masamix.c0m@hotmail.co.jp>
2019-05-13 12:35:30 +0000

    second commit

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+});
+
 Route::group(['prefix'=>'admin'],function(){
-    Route::get('news/create','Admin\NewsController@add')->middleware('auth');
+    Route::get('profile/edit','Admin\NewsController@add')->middleware('auth');
 });
 
 
 Route::get('admin/profile/create','Admin\ProfileController@add');
 
-Route::get('admin/profile/edit','Admin\ProfileController@edit');
+/*Route::get('admin/profile/edit','Admin\ProfileController@edit');*/
 
 Route::get('admin/profile/profile','Admin\ProfileController@edit');
 
